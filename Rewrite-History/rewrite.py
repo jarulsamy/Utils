9241f2c252c6f63adcb53853b92bec5e1ad6802e
Joshua Arulsamy <joshua.gf.arul@gmail.com>
2022-10-24 09:26:15 -0600

    Add timezone awareness

--- Rewrite-History/rewrite.py
+++ Rewrite-History/rewrite.py
@@ -1,8 +1,21 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+Rewrite git history specifying a specific timestamp for commits.
+
+usage: rewrite.py [-h] [-t TIMESTAMP] [-s SHELL] [-c]
+
+options:
+  -h, --help            show this help message and exit
+  -t TIMESTAMP, --timestamp TIMESTAMP
+                        Custom timestamp.
+  -s SHELL, --shell SHELL
+                        Custom shell
+  -c, --color           Enable color shell
+"""
 
 import argparse
 import os
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 from pathlib import Path
 
 ap = argparse.ArgumentParser()
@@ -36,7 +49,7 @@ if not args["shell"].exists:
 if not args["color"]:
     os.environ["TERM"] = "xterm-mono"
 
-ENV_VARS = ["GIT_COMMITTER_DATE", "GIT_AUTHOR_DATE"]
+env_vars = ("GIT_COMMITTER_DATE", "GIT_AUTHOR_DATE")
 
 if args["timestamp"] == "yesterday":
     date_ = datetime.today() - timedelta(days=1)
@@ -46,17 +59,21 @@ else:
     except ValueError:
         raise ValueError("Incorrect date format, should be YYYY-MM-DD")
 
-time = datetime.now()
-time = datetime.strftime(time, "%H:%M:%S")
-date_time = f"{datetime.strftime(date_, '%Y-%m-%d')} {time}"
+# Timezone aware datetimes are hard...
+# https://stackoverflow.com/a/25887393
+utc_dt = datetime.now(timezone.utc)
+dt = utc_dt.astimezone()
 
-for i in ENV_VARS:
+time_str = datetime.strftime(dt, "%H:%M:%S %z")
+date_time = f"{datetime.strftime(date_, '%Y-%m-%d')} {time_str}"
+
+for i in env_vars:
     os.environ[i] = date_time
 
 print("Dropping you into a new shell session.")
 print("Use exit to leave and unset the GIT variables.")
 
-for i in ENV_VARS:
+for i in env_vars:
     os.system(f"echo {i}: ${i}")
 
 os.system(args["shell"])
